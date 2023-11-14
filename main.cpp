0b0cddd962670be1ed4d13e8400cc333b811ff31
Charles Vazquez <crv@blites.com>
2023-11-14 08:57:28 -0600

    - I've made some minor changes in main.cpp to accomodate Qt style sheets.

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include "delayedexecutiontimer.h"
 #include "settingsmain.h"
 #include <QApplication>
+#include <QFile>
 
 
 
@@ -14,6 +15,11 @@ int main(int argc, char *argv[])
     /* The following statement prevents this error message:
      * QObject::connect: Cannot queue arguments of type 'QVector<int>'
      * (Make sure 'QVector<int>' is registered using qRegisterMetaType().) */
+
+    //QFile styleSheetFile("./Remover.qss");
+    //styleSheetFile.open(QFile::ReadOnly);
+    //QString styleSheet = QLatin1String(styleSheetFile.readAll());
+    //a.setStyleSheet(styleSheet);
 
     qRegisterMetaType<QVector<int>>();
 
